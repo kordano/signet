cc7e9630a54c6d1856d3065d92707244ce6b11b2
kordano <konrad.kuehne@rocketmail.com>
2015-02-19 18:49:02 +0100

    back to square 0

--- project.clj
+++ project.clj
@@ -11,6 +11,7 @@
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [com.taoensso/timbre "3.3.1"]
                  [net.polyc0l0r/konserve "0.2.3"]
+                 [aprint "0.1.3"]
                  [net.polyc0l0r/geschichte "0.1.0-SNAPSHOT"]]
   :main signet.core
   )

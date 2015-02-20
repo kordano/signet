7da35ca8ec8403885ae9a23b36eae03b83b08ece
kordano <konrad.kuehne@rocketmail.com>
2015-02-20 13:33:27 +0100

    some more tests

--- src/signet/core.clj
+++ src/signet/core.clj
@@ -1,6 +1,7 @@
 (ns signet.core
   (:require [taoensso.timbre :as timbre]
             [konserve.store :refer [new-mem-store]]
+            [konserve.protocols :refer [-get-in -assoc-in]]
             [geschichte.repo :as r]
             [geschichte.sync :refer [server-peer client-peer]]
             [geschichte.stage :as s]
@@ -14,7 +15,29 @@
 
 (comment
 
-  (def repo (r/new-repository "kordano@topiq.es" "Bookmark collector"))
+  (def store (<!? (new-mem-store)))
 
+  (def peer (client-peer "CLIENT" store identity))
+
+  (def stage (<!! (s/create-stage! "kordano@topiq.es" peer eval)))
+
+  (def r-id (<!? (s/create-repo! stage "Bookmark collection")))
+
+  (aprint r-id)
+
+  (<!? (s/transact stage ["kordano@topiq.es" r-id "master"]
+                   '(fn [old params] (merge old params))
+                   {:bookmark "q.es"}))
+
+
+  (<!? (s/commit! stage {"kordano@topiq.es" {r-id #{"master"}}}))
+
+
+  (<!? (-get-in store ["kordano@topiq.es"]))
+
+
+  (aprint (get-in @stage ["kordano@topiq.es" r-id]))
+
+  (aprint peer)
 
   )
